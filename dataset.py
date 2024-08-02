e3858a3ae10b84c2ff292c9314408046c256ffcb
Oshi16 <74727266+Oshi16@users.noreply.github.com>
2024-08-02 21:12:16 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,10 +1,5 @@
 # !usr/bin/env python
 # -*- coding:utf-8 _*-
-"""
-@Author: Huiqiang Xie
-@File: EurDataset.py
-@Time: 2021/3/31 23:20
-"""
 
 import os
 import pickle
@@ -17,23 +12,14 @@ class EurDataset(Dataset):
         with open(os.path.join(data_dir, 'europarl/{}_data.pkl'.format(split)), 'rb') as f:
             self.data = pickle.load(f)
 
-'''
-class EurDataset(Dataset):
-    def __init__(self, split='train'):
-        data_dir = './'
-        with open(data_dir + 'europarl/{}_data.pkl'.format(split), 'rb') as f:
-            self.data = pickle.load(f)
-'''
-
     def __getitem__(self, index):
         sents = self.data[index]
-        return  sents
+        return sents
 
     def __len__(self):
         return len(self.data)
 
 def collate_data(batch):
-
     batch_size = len(batch)
     max_len = max(map(lambda x: len(x), batch))   # get the max length of sentence in current batch
     sents = np.zeros((batch_size, max_len), dtype=np.int64)
@@ -43,4 +29,4 @@ def collate_data(batch):
         length = len(sent)
         sents[i, :length] = sent  # padding the questions
 
-    return  torch.from_numpy(sents)
+    return torch.from_numpy(sents)
